92c58ebf81ec9ae491ab20d9f0f6da971d06f46f
sunzongzheng <sunzongzhengwy@163.com>
2017-11-15 14:00:16 +0800

    修改构建配置

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 const shell = require('shelljs')
 
-shell.exec('npm run build:normal')
+shell.exec('npm run build-file && npm run build:normal')
 if (process.platform !== 'linux') {
   shell.exec('npm run build:win')
 }
