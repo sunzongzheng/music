ddecc075383807974af84d21c95220012f2c04c1
Suen <sunzongzhengwy@163.com>
2018-05-19 12:22:02 +0800

    mac状态栏歌词使用canvas画 [ci skip] (#8)

--- src/main/global/api.js
+++ src/main/global/api.js
@@ -1,71 +1,72 @@
 import api from 'music-api'
 
 export default {
-  ...api,
-  async searchSong(key) {
-    try {
-      let data = await api.searchSong(key)
-      if (data.status) {
-        data = data.data
-        // 为了保留 每个平台搜索结果 的排序权重  此处先找出最大长度
-        let maxLength = 0
-        for (let i in data) {
-          const len = data[i].songs.length
-          if (len > maxLength) {
-            maxLength = len
-          }
-        }
-        const result = []
-        for (let i = 0; i < maxLength; i++) {
-          for (let j in data) {
-            const cur = data[j].songs[i]
-            // 如果存在
-            if (typeof cur !== 'undefined') {
-              // 检查是否已存在同名 同歌手 同专辑 则跳过
-              if (result.filter(item => {
-                  // 首先判断歌手是不是全等 此处歌手可能有多个
-                  let art_same = true
-                  item.artists.every((item, index) => {
-                    if (!cur.artists[index] || item.name !== cur.artists[index].name) {
-                      art_same = false
-                      return false
+    ...api,
+    async searchSong(key) {
+        try {
+            let data = await api.searchSong(key)
+            if (data.status) {
+                data = data.data
+                // 为了保留 每个平台搜索结果 的排序权重  此处先找出最大长度
+                let maxLength = 0
+                for (let i in data) {
+                    const len = data[i].songs.length
+                    if (len > maxLength) {
+                        maxLength = len
                     }
-                    return true
-                  })
-                  return item.name === cur.name && art_same && item.album.name === cur.album.name
-                }).length) {
-                continue
-              } else {
-                result.push({
-                  ...cur,
-                  source: j
-                })
-              }
+                }
+                const result = []
+                for (let i = 0; i < maxLength; i++) {
+                    for (let j in data) {
+                        const cur = data[j].songs[i]
+                        // 如果存在
+                        if (typeof cur !== 'undefined') {
+                            // 检查是否已存在同名 同歌手 同专辑 则跳过
+                            if (result.filter(item => {
+                                // 首先判断歌手是不是全等 此处歌手可能有多个
+                                let art_same = true
+                                item.artists.every((item, index) => {
+                                    if (!cur.artists[index] || item.name !== cur.artists[index].name) {
+                                        art_same = false
+                                        return false
+                                    }
+                                    return true
+                                })
+                                return item.name === cur.name && art_same && item.album.name === cur.album.name
+                            }).length) {
+                                continue
+                            } else {
+                                result.push({
+                                    ...cur,
+                                    source: j
+                                })
+                            }
+                        }
+                    }
+                }
+                return {
+                    status: true,
+                    data: result
+                }
+            } else {
+                return data
             }
-          }
+        } catch (e) {
+            console.warn(e)
+            return {
+                status: false,
+                msg: e.msg || '搜索失败，请重试',
+                log: e
+            }
         }
-        return {
-          status: true,
-          data: result
+    },
+    async getTopList(id) {
+        const data = await api.getTopList(id)
+        if (data.status) {
+            for (let item of data.data.list) {
+                item.source = 'netease'
+            }
         }
-      } else {
         return data
-      }
-    } catch (e) {
-      return {
-        status: false,
-        msg: e.msg || '搜索失败，请重试',
-        log: e
-      }
     }
-  },
-  async getTopList(id) {
-    const data = await api.getTopList(id)
-    if (data.status) {
-      for (let item of data.data.list) {
-        item.source = 'netease'
-      }
-    }
-    return data
-  }
 }
---
+++ src/renderer/lyrics.js
@@ -0,0 +1,94 @@
+const {nativeImage, remote} = require("electron")
+const setTray = remote.getGlobal('setTray')
+
+module.exports = {
+    canvas: null,
+    ctx: null,
+    devicePixelRatio: 2,
+    w: 175,
+    h: 18,
+    fontSize: 14,
+    lyric: {
+        text: '听你想听的音乐',
+        width: 0,
+        time: 0 // 单句歌词的播放时间
+    },
+    x: 0, // 移动的距离
+    timer: null,
+    frame: 60,
+
+    init() {
+        this.canvas = document.createElement('canvas')
+        this.canvas.width = this.w * this.devicePixelRatio
+        this.canvas.height = this.h * this.devicePixelRatio
+        this.ctx = this.canvas.getContext('2d')
+        this.ctx.font = `${this.fontSize * this.devicePixelRatio}px "microsoft yahei", sans-serif`
+        this.ctx.textBaseline = 'middle'
+        this.updateLyric()
+    },
+    updateLyric(arg = this.lyric) {
+        clearInterval(this.timer)
+        this.x = 0
+        const measureText = this.ctx.measureText(arg.text)
+        this.lyric = {
+            text: arg.text,
+            width: measureText.width,
+            time: arg.time
+        }
+        if (this.lyric.width > this.canvas.width) {
+            // 计算第一屏文字占总文字长度的比率
+            const rate = this.canvas.width / this.lyric.width
+            // 根据比率计算出第一屏文字静止的时间
+            const staticTime = Math.min(rate * this.lyric.time, 2000)
+            // 渲染第一屏文字
+            this.draw()
+            // 延时move
+            setTimeout(() => {
+                // 开始移动
+                this.timer = setInterval(() => {
+                    this.move()
+                    this.draw()
+                }, 1000 / this.frame)
+            }, staticTime)
+            // 取消
+            setTimeout(() => {
+                clearInterval(this.timer)
+            }, this.lyric.time)
+        } else {
+            this.draw()
+        }
+    },
+    move() {
+        // 计算文字超出canvas的部分
+        const more = this.lyric.width - this.canvas.width
+        // 文字右侧没有到canvas右侧
+        if (-this.x < more) {
+            // 计算超出文字占总文字长度的比率
+            const rate = more / this.lyric.width
+            // 根据比率 计算出超出文字滚动需要的时间
+            const scrollTime = rate * this.lyric.time
+            // 根据时间 计算出每帧需要移动的距离
+            const distance = more / Math.max(this.lyric.time - 2000, scrollTime) * this.frame
+            this.x -= distance * this.devicePixelRatio
+        }
+    },
+    draw() {
+        let x
+        if (this.lyric.width <= this.canvas.width) {
+            x = this.canvas.width / 2
+            this.ctx.textAlign = 'center'
+        } else {
+            x = this.x
+            this.ctx.textAlign = 'left'
+        }
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+        this.ctx.fillText(this.lyric.text, x, this.canvas.height / 2 + 2)
+        setTray(this.canvas.toDataURL(), this.w, this.h)
+    },
+    getImage() {
+        return nativeImage.createFromDataURL(this.canvas.toDataURL()).resize({
+            width: this.w,
+            height: this.h
+        })
+    }
+}
