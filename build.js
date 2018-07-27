dd6f39f540704a960a4bb3f6b8a1658df8878eba
sunzongzheng <sunzongzhengwy@163.com>
2018-07-27 18:00:33 +0800

    fix: build.js不会打包windows [ci skip]

--- build.js
+++ build.js
@@ -1,6 +1,13 @@
-const { exec } = require('child_process')
+const {exec} = require('child_process')
 
-exec('npm run build-file && npm run build:normal')
-if (process.platform !== 'linux') {
-  exec('npm run build:win')
-}
+exec('npm run build-file && npm run build:normal', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`exec error: ${error}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    if (process.platform !== 'linux') {
+        exec('npm run build:win')
+    }
+})
