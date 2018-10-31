d8ea50c669299812c916fa9d922c31b64769b777
sunzongzheng <sunzongzhengwy@163.com>
2018-10-31 13:25:21 +0800

    本地歌曲扫描加try catch

--- src/renderer/store/modules/local-songs.js
+++ src/renderer/store/modules/local-songs.js
@@ -23,44 +23,48 @@ export default {
             commit('clear')
             const folders = Vue.$store.state.user.setting.localSongsFolders
             for (let folder of folders) {
-                const dirs = fs.readdirSync(folder)
-                for (let item of dirs) {
-                    const pathname = path.join(folder, item)
-                    const stat = fs.statSync(pathname)
-                    if (stat.isFile()) {
-                        if (item.endsWith('.mp3') || item.endsWith('flac')) {
-                            const metadata = await mm.parseFile(pathname)
-                            if (metadata.common.title === '可不可以') {
-                                console.log(metadata)
+                try {
+                    const dirs = fs.readdirSync(folder)
+                    for (let item of dirs) {
+                        const pathname = path.join(folder, item)
+                        const stat = fs.statSync(pathname)
+                        if (stat.isFile()) {
+                            if (item.endsWith('.mp3') || item.endsWith('flac')) {
+                                const metadata = await mm.parseFile(pathname)
+                                if (metadata.common.title === '可不可以') {
+                                    console.log(metadata)
+                                }
+                                const bitrate = metadata.format.bitrate || 128000
+                                commit('add', {
+                                    name: item.substring(0, item.lastIndexOf('.')),
+                                    album: {
+                                        name: metadata.common.album,
+                                    },
+                                    artists: metadata.common.artists ? metadata.common.artists.map(item => {
+                                        return {
+                                            name: item,
+                                        }
+                                    }) : [],
+                                    cp: false,
+                                    quality: {
+                                        192: (bitrate / 1000) === 192,
+                                        320: (bitrate / 1000) === 320,
+                                        999: (bitrate / 1000) === 999,
+                                    },
+                                    songId: uuid(),
+                                    bitrate,
+                                    duration: parseInt(metadata.format.duration),
+                                    fullpath: pathname,
+                                    folder,
+                                    size: stat.size,
+                                })
                             }
-                            const bitrate = metadata.format.bitrate || 128000
-                            commit('add', {
-                                name: item.substring(0, item.lastIndexOf('.')),
-                                album: {
-                                    name: metadata.common.album,
-                                },
-                                artists: metadata.common.artists ? metadata.common.artists.map(item => {
-                                    return {
-                                        name: item,
-                                    }
-                                }) : [],
-                                cp: false,
-                                quality: {
-                                    192: (bitrate / 1000) === 192,
-                                    320: (bitrate / 1000) === 320,
-                                    999: (bitrate / 1000) === 999,
-                                },
-                                songId: uuid(),
-                                bitrate,
-                                duration: parseInt(metadata.format.duration),
-                                fullpath: pathname,
-                                folder,
-                                size: stat.size,
-                            })
+                        } else if (stat.isDirectory()) {
+                            this.refresh(pathname)
                         }
-                    } else if (stat.isDirectory()) {
-                        this.refresh(pathname)
                     }
+                } catch (e) {
+                    console.warn(e)
                 }
             }
         },
