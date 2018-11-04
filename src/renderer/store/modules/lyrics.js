9a0a5870cabb458b9f9308aa3da3d44b63f35412
sunzongzheng <sunzongzhengwy@163.com>
2018-11-04 11:04:38 +0800

    Fix: 首末插入行导致没有歌词时数组长度也不为0

--- src/renderer/store/modules/lyrics.js
+++ src/renderer/store/modules/lyrics.js
@@ -8,26 +8,28 @@ export default {
         translate: [],
         loading: false,
         activeIndex: 0,
-        showTranslate: false
+        showTranslate: false,
     },
     mutations: {
         update(state, val) {
             for (let i in val) {
                 state[i] = val[i]
             }
-        }
+        },
     },
     actions: {
-        async init({commit}) {
+        async init({ commit }) {
             const playInfo = Vue.$store.state.play.info
             commit('update', {
-                loading: true
+                loading: true,
             })
             const data = await Vue.$musicApi.getLyric(playInfo.vendor, playInfo.songId)
             const startsArr = new Array(6).fill(null).map(item => ['0:0.0', ' '])
             const endsArr = new Array(6).fill(null).map(item => ['99:99.0', ' '])
+            const lyrics = data.status ? data.data.lyric : []
+            const translate = data.status ? data.data.translate : []
             commit('update', {
-                lyrics: startsArr.concat((data.status ? data.data.lyric : [])).concat(endsArr).map(item => {
+                lyrics: (lyrics.length ? startsArr.concat(lyrics).concat(endsArr) : []).map(item => {
                     const lyric = [...item] // 避免污染startsArr,endsArr中的值
                     let arr = lyric[0].match(/^(\d+):(\d+).(\d+)$/)
                     if (arr) {
@@ -35,7 +37,7 @@ export default {
                     }
                     return lyric
                 }),
-                translate: startsArr.concat((data.status ? data.data.translate : [])).concat(endsArr).map(item => {
+                translate: (translate.length ? startsArr.concat(translate).concat(endsArr) : []).map(item => {
                     let arr = item[0].match(/^(\d+):(\d+).(\d+)$/)
                     if (arr) {
                         item[0] = parseInt(arr[1]) * 60 * 1000 + parseInt(arr[2]) * 1000 + parseInt(arr[3].padEnd(3, '0'))
@@ -43,13 +45,13 @@ export default {
                     return item
                 }),
                 loading: false,
-                activeIndex: 0
+                activeIndex: 0,
             })
-        }
+        },
     },
     getters: {
         hasTranslation(state) {
             return Boolean(state.translate.length)
-        }
-    }
+        },
+    },
 }
