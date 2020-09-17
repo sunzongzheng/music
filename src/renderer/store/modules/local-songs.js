9e91891dae9e403759e914871db0f8cc1b59fbe9
sunzongzheng <sunzongzhengwy@163.com>
2020-09-17 20:09:18 +0800

    feat: 本地歌曲递归扫描 resolved #107

--- src/renderer/store/modules/local-songs.js
+++ src/renderer/store/modules/local-songs.js
@@ -22,45 +22,56 @@ export default {
         async refresh({ commit }) {
             commit('clear')
             const folders = Vue.$store.state.user.setting.localSongsFolders
+
+            const walk = async folder => {
+                const dirs = fs.readdirSync(folder)
+                for (let filename of dirs) {
+                    const filepath = path.join(folder, filename)
+                    const stat = fs.statSync(filepath)
+                    if (stat.isFile()) {
+                        if (
+                            filename.endsWith('.mp3') ||
+                            filename.endsWith('flac')
+                        ) {
+                            const metadata = await mm.parseFile(filepath)
+                            const bitrate = metadata.format.bitrate || 128000
+                            commit('add', {
+                                name: filename.substring(
+                                    0,
+                                    filename.lastIndexOf('.')
+                                ),
+                                album: {
+                                    name: metadata.common.album,
+                                },
+                                artists: metadata.common.artists
+                                    ? metadata.common.artists.map(name => {
+                                          return {
+                                              name,
+                                          }
+                                      })
+                                    : [],
+                                cp: false,
+                                quality: {
+                                    192: bitrate / 1000 === 192,
+                                    320: bitrate / 1000 === 320,
+                                    999: bitrate / 1000 === 999,
+                                },
+                                songId: uuid(),
+                                bitrate,
+                                duration: parseInt(metadata.format.duration),
+                                fullpath: filepath,
+                                folder,
+                                size: stat.size,
+                            })
+                        }
+                    } else if (stat.isDirectory()) {
+                        await walk(filepath)
+                    }
+                }
+            }
             for (let folder of folders) {
                 try {
-                    const dirs = fs.readdirSync(folder)
-                    for (let item of dirs) {
-                        const pathname = path.join(folder, item)
-                        const stat = fs.statSync(pathname)
-                        if (stat.isFile()) {
-                            if (item.endsWith('.mp3') || item.endsWith('flac')) {
-                                const metadata = await mm.parseFile(pathname)
-                                if (metadata.common.title === '可不可以') {
-                                    console.log(metadata)
-                                }
-                                const bitrate = metadata.format.bitrate || 128000
-                                commit('add', {
-                                    name: item.substring(0, item.lastIndexOf('.')),
-                                    album: {
-                                        name: metadata.common.album,
-                                    },
-                                    artists: metadata.common.artists ? metadata.common.artists.map(item => {
-                                        return {
-                                            name: item,
-                                        }
-                                    }) : [],
-                                    cp: false,
-                                    quality: {
-                                        192: (bitrate / 1000) === 192,
-                                        320: (bitrate / 1000) === 320,
-                                        999: (bitrate / 1000) === 999,
-                                    },
-                                    songId: uuid(),
-                                    bitrate,
-                                    duration: parseInt(metadata.format.duration),
-                                    fullpath: pathname,
-                                    folder,
-                                    size: stat.size,
-                                })
-                            }
-                        }
-                    }
+                    await walk(folder)
                 } catch (e) {
                     console.warn(e)
                 }
