12df93d658d346e8c5a5485045b36ecf1a3f167d
sunzongzheng <sunzongzhengwy@163.com>
2020-09-19 00:12:30 +0800

    fix: 对接口数据做兼容处理 resolved #101

--- src/renderer/store/modules/lyrics.js
+++ src/renderer/store/modules/lyrics.js
@@ -23,24 +23,44 @@ export default {
             commit('update', {
                 loading: true,
             })
-            const data = await Vue.$musicApi.getLyric(playInfo.vendor, playInfo.songId)
-            const startsArr = new Array(6).fill(null).map(item => ['0:0.0', ' '])
-            const endsArr = new Array(6).fill(null).map(item => ['99:99.0', ' '])
-            const lyrics = data.status ? data.data.lyric : []
-            const translate = data.status ? data.data.translate : []
+            const data = await Vue.$musicApi.getLyric(
+                playInfo.vendor,
+                playInfo.songId
+            )
+            const startsArr = new Array(6)
+                .fill(null)
+                .map(item => ['0:0.0', ' '])
+            const endsArr = new Array(6)
+                .fill(null)
+                .map(item => ['99:99.0', ' '])
+            const lyrics = data.status && data.data.lyric ? data.data.lyric : []
+            const translate =
+                data.status && data.data.translate ? data.data.translate : []
             commit('update', {
-                lyrics: (lyrics.length ? startsArr.concat(lyrics).concat(endsArr) : []).map(item => {
+                lyrics: (lyrics.length
+                    ? startsArr.concat(lyrics).concat(endsArr)
+                    : []
+                ).map(item => {
                     const lyric = [...item] // 避免污染startsArr,endsArr中的值
                     let arr = lyric[0].match(/^(\d+):(\d+).(\d+)$/)
                     if (arr) {
-                        lyric[0] = parseInt(arr[1]) * 60 * 1000 + parseInt(arr[2]) * 1000 + parseInt(arr[3].padEnd(3, '0'))
+                        lyric[0] =
+                            parseInt(arr[1]) * 60 * 1000 +
+                            parseInt(arr[2]) * 1000 +
+                            parseInt(arr[3].padEnd(3, '0'))
                     }
                     return lyric
                 }),
-                translate: (translate.length ? startsArr.concat(translate).concat(endsArr) : []).map(item => {
+                translate: (translate.length
+                    ? startsArr.concat(translate).concat(endsArr)
+                    : []
+                ).map(item => {
                     let arr = item[0].match(/^(\d+):(\d+).(\d+)$/)
                     if (arr) {
-                        item[0] = parseInt(arr[1]) * 60 * 1000 + parseInt(arr[2]) * 1000 + parseInt(arr[3].padEnd(3, '0'))
+                        item[0] =
+                            parseInt(arr[1]) * 60 * 1000 +
+                            parseInt(arr[2]) * 1000 +
+                            parseInt(arr[3].padEnd(3, '0'))
                     }
                     return item
                 }),
