a4bb8080e1633a0768f352d998467a6f1ffe6594
libook <libook7@gmail.com>
2021-07-14 14:20:37 +0800

    feat: check the version of the restoring file

--- src/renderer/view/setting/components/debug.vue
+++ src/renderer/view/setting/components/debug.vue
@@ -21,10 +21,17 @@
 </template>
 <script>
 import menuItem from './menu-item.vue'
+import { remote } from 'electron'
+import compareVersions from 'compare-versions'
 
 export default {
     components: {
         menuItem,
+    },
+    data() {
+        return {
+            RESTORE_SUPPORT_VERSION_FROM: '1.3.1',
+        }
     },
     methods: {
         openConsole() {
@@ -33,10 +40,14 @@ export default {
             })
         },
         backup() {
-            const data = new Blob([JSON.stringify(localStorage, null, 2)], {
+            const backupObject = {
+                version: remote.app.getVersion(),
+                data: localStorage,
+            }
+            const jsonFile = new Blob([JSON.stringify(backupObject, null, 2)], {
                 type: 'application/json',
             })
-            const url = URL.createObjectURL(data)
+            const url = URL.createObjectURL(jsonFile)
             const aElement = document.createElement('a')
             aElement.href = url
             aElement.download = 'backup.json'
@@ -47,6 +58,7 @@ export default {
         },
         readSingleFile(changeEvent) {
             const file = changeEvent.target.files[0]
+            const thisComponent = this
             if (file) {
                 const reader = new FileReader()
                 reader.onload = function(loadEvent) {
@@ -55,11 +67,30 @@ export default {
                      */
                     const contents = loadEvent.target.result
                     try {
-                        const restoreJson = JSON.parse(contents)
-                        for (const key in restoreJson) {
-                            localStorage.setItem(key, restoreJson[key])
+                        const backupObject = JSON.parse(contents)
+
+                        // Check if the version is not correct
+                        if (
+                            !backupObject.version || // version field not exists
+                            compareVersions(
+                                backupObject.version,
+                                thisComponent.RESTORE_SUPPORT_VERSION_FROM
+                            ) < 0 || // version is less than RESTORE_SUPPORT_VERSION_FROM
+                            compareVersions(
+                                backupObject.version,
+                                remote.app.getVersion()
+                            ) > 0 // version is greater than current version
+                        ) {
+                            throw new Error(`不支持导入这个版本的数据！`)
+                        } // TODO support convert old version
+                        if (typeof backupObject.data === 'object') {
+                            for (const key in backupObject) {
+                                localStorage.setItem(key, backupObject[key])
+                            }
+                            alert('导入成功！请重启应用。')
+                        } else {
+                            alert('数据格式错误！')
                         }
-                        alert('导入成功！请重启应用。')
                     } catch (error) {
                         alert(error)
                     }
